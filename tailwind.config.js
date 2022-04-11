4ba4f19ec8d42cf5f636c1c16ef8349f787e1e7c
juli6464 <juli6464@hotmail.com>
2022-04-11 10:46:03 -0500

    first upload

---
+++ tailwind.config.js
@@ -0,0 +1,40 @@
+module.exports = {
+  content: ["./public/index.html", "./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        sanFrancisco: "url('../img/sanFrancisco.jpg')",
+        sanFranciscoDesktop: "url('../img/sanFranciscoDesktop.jpg')",
+        yosemite: "url('../img/yosemite.jpg')",
+        LA: "url('../img/LA.jpg')",
+        seattle: "url('../img/seattle.jpg')",
+        new_york: "url('../img/new_york.jpg')",
+        norway: "url('../img/norway.jpg')",
+        sydney: "url('../img/sydney.jpg')",
+        miami: "url('../img/miami.jpg')",
+        switzerland: "url('../img/switzerland.jpg')",
+        bali: "url('../img/bali.jpg')",
+        chicago: "url('../img/chicago.jpg')",
+        europe: "url('../img/europe.jpg')",
+        iceland: "url('../img/iceland.jpg')",
+      },
+      backgroundColor: theme => ({
+        primary: '#cc2d4b',
+        secondary: '#8fa206',
+        terciary: '#61AEC9',
+      }),
+      textColor: theme => ({
+        primary: '#cc2d4b',
+        secondary: '#8fa206',
+        terciary: '#61AEC9',
+      }),
+      fontFamily: {
+        Montserrat: ['Montserrat', 'sans-serif'],
+      }
+  },
+  variants: {
+    width: ['responsive', 'hover', 'focus'],
+  },
+  plugins: [],
+}
+}
